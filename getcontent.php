8881123a5db6838b7d60186d568d9d14c0d83430
PavelUdovichenko <Udovichenkop@gmail.com>
2021-12-28 07:31:48 +0300

    Add files via upload
    довел до ума получение всех необходимых фактов для дальнейшего преобразования расписания

--- getcontent.php
+++ getcontent.php
@@ -1,24 +1,32 @@
 <?
-//include ('g-auth.php');
+header('Location: page.html');
+
 $name=$_SERVER['PHP_SELF'];
-$type = $_POST['type'];
+$type = $_POST['user'];
 $gnumber = $_POST['gnumber'];
 $Tname = $_POST['Tname'];
 $infofile = "task.txt";
 $abbr1 = $_POST['faculties'];
-	
-//echo "$type \r\n $gnumber \r\n $Tname \r\n $abbr1  ";
+$sdate = $_POST['startdate'];
+$edate = $_POST['enddate'];
+$comment = $_POST['comment'];
+
+echo "$type \r\n $gnumber \r\n $Tname \r\n $abbr1 $sdate  $edate $comment ";
+if(isset($_POST['Ntf'])){
+	$ntf = 'True';
+}
+else $ntf = 'False';
 
 if (!file_exists($infofile)) 
 	{
 		$file = fopen($infofile,"w+");
 		if (!$Tname)
 		{
-			fwrite($file, $type ."\r\n" .$gnumber."\r\n".$abbr1);
+			fwrite($file, $type ."\r\n" .$gnumber."\r\n".$abbr1."\r\n".$sdate."\r\n".$edate."\r\n".$ntf."\r\n".$comment);
 		}
 		else
 		{
-			fwrite($file, $type ."\r\n" .$Tname."\r\n".$abbr1);
+			fwrite($file, $type ."\r\n" .$Tname."\r\n".$abbr1."\r\n".$sdate."\r\n".$edate."\r\n".$ntf."\r\n".$comment);
 		}
 			
 		fclose($file);
@@ -30,17 +38,21 @@ else
 		file_put_contents($infofile, ' ');
 		if (!$Tname)
 		{
-			fwrite($file, $type ."\r\n".$gnumber."\r\n" .$abbr1);	
+			fwrite($file, $type ."\r\n".$gnumber."\r\n" .$abbr1."\r\n".$sdate."\r\n".$edate."\r\n".$ntf."\r\n".$comment);	
 		}
 		else
 		{
-			fwrite($file, $type ."\r\n" .$Tname."\r\n" .$abbr1);
+			fwrite($file, $type ."\r\n" .$Tname."\r\n" .$abbr1."\r\n".$sdate."\r\n".$edate."\r\n".$ntf."\r\n".$comment);
 		}
 					
 		fclose($file);
 	}
+echo(shell_exec('python events_try1.py 2>&1'));
 	
-	shell_exec('python geturlcontent.py 2>&1');
-	
-	
+
+# echo "<HTML><HEAD>
+
+# <META HTTP-EQUIV='Refresh' CONTENT='0; URL=page.html'>
+
+# </HEAD></HTML>";
 ?>
